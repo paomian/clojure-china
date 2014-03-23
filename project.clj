7f31bb22a49e9cec44eb0338fda0f9d704cee6b4
paomian <tanglnan1994@gmail.com>
2014-03-23 20:14:13 +0800

    first commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject clojure-china "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.6"]]
+  :plugins [[lein-ring "0.8.10"]]
+  :ring {:handler clojure-china.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})

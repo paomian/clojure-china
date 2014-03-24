efc219c8b754f026efaa53e4b2d105dd82a3ea36
gorup <tanglnan1994@gmail.com>
2014-03-24 20:19:04 +0800

    change project

--- project.clj
+++ project.clj
@@ -1,8 +1,15 @@
 (defproject clojure-china "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "This is the source code of Clojure China website."
+  :url "http://clojure-china.org"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]]
+                 [compojure "1.1.6"]
+                 [org.clojure/java.jdbc "0.3.3"] ;jdbc
+                 [postgresql/postgresql "8.4-702.jdbc4"] ;pgsql client
+                 [com.taoensso/carmine "2.4.6"] ;redis client
+                 [lib-noir "0.8.1"] ;A set of utilities and helpers for building ring apps.
+                 [org.jasypt/jasypt "1.7"] ;JAVA加密包
+                 [hiccup "1.0.5"]
+                 ]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler clojure-china.handler/app}
   :profiles

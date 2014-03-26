18702dde7643354833671fd9da41aa7d66cbd7a5
gorup <tanglnan1994@gmail.com>
2014-03-26 17:46:10 +0800

    modified:   src/clojure_china/dbutil/userdbutil.clj

--- src/clojure_china/dbutil/userdbutil.clj
+++ src/clojure_china/dbutil/userdbutil.clj
@@ -1,10 +1,6 @@
 ;;对于用户的个人信息的一起列操作方法
-(ns clojure-china.dbutil
-  (:require   [clojure.java.jdbc  :as jdbc]))
-;;连接数据库
-(def pgsql-db {:subprotool "postgre"
-               :subname "/127.0.0.1:3306/clojure-china"
-               :user "clojure-china"
-               :password "clojure-china"})
+(ns clojure-china.dbutil.dbutil
+  (:use [clojure-china.dbutil.dbconn :only db-postgres])
+  (:require [clojure.java.jdbc  :as jdbc]))
 ;;插入数据
-(defn userinsert! [table ])
+(defn userinsert! [table db])

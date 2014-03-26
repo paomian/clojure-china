0e9b6f6478e6e397f7311e0ca3b25fef9e9b402a
gorup <tanglnan1994@gmail.com>
2014-03-27 02:47:01 +0800

    update dbconf

--- src/clojure_china/dbutil/README.md
+++ src/clojure_china/dbutil/README.md
@@ -3,6 +3,10 @@
 - In order to protect the database password, we omitted a file called dbconn.clj
 - It is look like this
 
+###Ation
+
+- Befor you run the clojure-china,you must run the script cc_user.sql to init databases; 
+
 ```clojure
 (ns clojure-china.dbconn)
 
---
+++ src/clojure_china/dbutil/cc_user.sql
@@ -0,0 +1,10 @@
+create table cc_user
+(
+	id serial,
+	username varchar(20) not null,
+	password varchar(100) not null,
+	email varchar(20) not null,
+	is_admin boolean not null,
+	register_time timestamp without time zone,
+	last_login_time timestamp without time zone
+);

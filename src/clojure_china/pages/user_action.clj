8651deebb21cc6cc187612877b40ffa437c19efe
gorup <tanglnan1994@gmail.com>
2014-04-02 16:12:11 +0800

    modified:   user_action.clj

--- src/clojure_china/pages/user_action.clj
+++ src/clojure_china/pages/user_action.clj
@@ -4,7 +4,36 @@
             [hiccup.page :refer :all]
             [hiccup.form :refer :all]
             [clojure-china.pages.htmlutil :refer [def-page]]))
+
 (def register-form
+  [:form {:role "form" :action "/register" :method "post"}
+   [:div {:class "form-group"}
+    [:label {:for "username"} "用户名"]
+    [:input {:type "text" :class "form-control" :id "username" :name "username" :placeholder "用户名请用英文及数字，长度
+请勿超过20个字符"}]]
+   [:div {:class "form-group"}
+    [:label {:for "password"} "密码"]
+    [:input {:type "password" :class "form-control" :id "password" :name "password"}]]
+   [:div {:class "form-group"}
+    [:label {:for "password"} "密码确认"]
+    [:input {:type "password" :class "form-control" :id "rpassword" :name "rpassword"}]]
+   [:div {:class "form-group"}
+    [:label {:for "email"} "Email地址"]
+    [:input {:type "email" :class "form-control" :id "email" :name "email"}]]
+   [:button {:type "submit" :class "btn btn-default" }"注册"]])
+
+(def register-modal
+  [:div {:class "modal fade" :id "register" :tabindex "-1" :role "dialog" :aria-labelledby "注册会员" :aria-hidden "true"}
+   [:div {:class "modal-dialog"}
+    [:div {:class "modal-content"}
+     [:div {:class "modal-header"}
+      [:button {:type "button" :class "close" :data-dismiss "modal" :aria-hidden "true"} "&times;"]
+      [:h4 {:class "modal-title" :id "registerLabel"} "注册会员"]]
+     [:div {:class "modal-body"}
+      registr-form]]]])
+
+
+#_(def register-form
   [:form.form-signin {:role "form" :method "POST" :action "/register"}
    [:table
     [:tr
@@ -27,7 +56,8 @@
           {:hiccup (list
                      (session/flash-get :error)
                      (session/flash-get :success)
-                     register-form)
+                     register-modal)
+                     ;;register-form)
            :title "register"})
 
 (def login-form

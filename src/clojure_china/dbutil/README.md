69b7c9c977a961158ea9bdb9ad144746ee38a786
paomian <tanglnan1994@gmail.com>
2014-08-13 17:26:10 +0800

    add conn pool

--- src/clojure_china/dbutil/README.md
+++ src/clojure_china/dbutil/README.md
@@ -16,6 +16,26 @@
    :subname (str "//" db-host ":" db-port "/" db-name)
    :user db-user
    :password "your db password"})
+   
+(defn pool
+  "创建数据库连接池"
+  [spec]
+  (let [cpds (doto (ComboPooledDataSource.)
+               (.setDriverClass (:classname spec))
+               (.setJdbcUrl (str "jdbc:" (:subprotocol spec) ":" (:subname spec)))
+               (.setUser (:user spec))
+               (.setPassword (:password spec))
+               ;; expire excess connections after 30 minutes of inactivity:
+               (.setMaxIdleTimeExcessConnections (* 30 60))
+               ;; expire connections after 3 hours of inactivity:
+               (.setMaxIdleTime (* 3 60 60)))]
+    {:datasource cpds}))
+
+(def pooled-db (delay (pool db-spec)))
+
+(defn db-connection
+  []
+  @pooled-db)
 ```
 
 ###Ation
--- src/clojure_china/dbutil/account/dbutil.clj
+++ src/clojure_china/dbutil/account/dbutil.clj
@@ -1,50 +1,53 @@
 ;;对于用户的个人信息的一起列操作方法
 (ns clojure-china.dbutil.account.dbutil
   (:require [clojure.java.jdbc :as jdbc]
-            [clojure-china.dbutil.dbconn :refer [db-spec]]
+            [clojure-china.dbutil.dbconn :refer [db-connection]]
             [clj-time.local :as l])
-  (:import  [java.sql Timestamp]
-            [java.util Date]))
+  (:import [java.sql Timestamp]
+           [java.util Date]))
 
 ;;(defrecord User [])
 ;;按id查询用户
 (defn user-id-query [id]
   "按照ID查询用户"
-  (jdbc/query db-spec 
-              ["SELECT * FROM CC_USER WHERE ID = ?" id]))
+  (jdbc/query
+    (db-connection)
+    ["SELECT * FROM CC_USER WHERE ID = ?" id]))
 ;;按用户名查询用户
 (defn user-name-query
   [username]
-  (jdbc/query db-spec 
-              ["SELECT * FROM CC_USER WHERE USERNAME = ?" username]))
+  (jdbc/query
+    (db-connection)
+    ["SELECT * FROM CC_USER WHERE USERNAME = ?" username]))
 (defn check-username
   [username]
-  (empty? (jdbc/query db-spec
-                  ["SELECT USERNAME FROM CC_USER WHERE USERNAME = ?" username])))
+  (empty? (jdbc/query
+            (db-connection)
+            ["SELECT USERNAME FROM CC_USER WHERE USERNAME = ?" username])))
 ;;新建用户
 (defn user-create!
   [username encrypted-password email is-admin?]
   (jdbc/insert!
-    db-spec :cc_user
-    {:username username
-     :password encrypted-password
-     :email email
-     :is_admin is-admin?
+    (db-connection) :cc_user
+    {:username        username
+     :password        encrypted-password
+     :email           email
+     :is_admin        is-admin?
      :last_login_time (Timestamp. (.getTime (Date.)))
-     :register_time (Timestamp. (.getTime (Date.)))}))
+     :register_time   (Timestamp. (.getTime (Date.)))}))
 
 (defn user-update!
   [username encrypted-password email]
-  (jdbc/update!  db-spec :cc_user
+  (jdbc/update!
+    (db-connection) :cc_user
     {:password encrypted-password
-     :email email
-     }
-    ["USERNAME = ?" username]
-    ))
+     :email    email}
+    ["USERNAME = ?" username]))
 
 #_(defn user-update
   [])
 ;;更新用户最后登录时间
 (defn user-update-lastlogintime
   [userid]
-  (jdbc/update! db-spec :cc_user {:last_login_time (Timestamp. (.getTime (Date.)))} ["ID=?" userid]))
+  (jdbc/update! (db-connection) :cc_user
+                {:last_login_time (Timestamp. (.getTime (Date.)))} ["ID=?" userid]))

a6f05ba057cadf452e9f293bb9e75ef99f1a877d
lidashuang <ldshuang@gmail.com>
2014-08-22 21:58:20 +0800

    add config.clj

---
+++ src/clojure_china/config.clj
@@ -0,0 +1,14 @@
+(ns clojure-china.config)
+
+;; Get the application env keyword, default is :dev
+(defn env []
+  (let [env (System/getenv "CC_ENV")]
+  (if env (keyword env) :dev)))
+
+(defn database-config []
+  ((env) {
+    :dev  {:name "cc_dev", :user "vagrant", :password "root", :host "127.0.0.1", :port "5432"},
+    :test {:name "cc_test", :user "vagrant", :password "root", :host "127.0.0.1", :port "5432"}, 
+    :prod {:name "cc_prod", :user "vagrant", :password "root", :host "127.0.0.1", :port "5432"}}))
+
+(def database-url "jdbc:postgresql://localhost:5432/cc?user=vagrant&password=root")

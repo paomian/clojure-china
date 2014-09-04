eeddbd221678e3559f9165ce44b1829285f0cd28
paomian <xpaomian@gmail.com>
2014-09-04 21:38:58 +0800

    fix bug and change the post controller

--- src/clojure_china/model/account/account.clj
+++ src/clojure_china/model/account/account.clj
@@ -1,32 +1,21 @@
 (ns clojure-china.model.account.account
   "对于用户的一些列操作"
   (:require [clojure.java.jdbc :as jdbc]
-            [korma
-             [core :as k]]                                  ;todo
             [clj-time.local :as l]
             [clojure-china.model.dbutil :as db]
-            [clojure-china.model.entitys :refer :all])
-  (:import [java.sql Timestamp]
-           [java.util Date]))
+            [clojure-china.model.entitys :refer :all]))
 
 
 
-(defn id-query
+(defn id
   "按照ID查询用户"
   [^Integer id]
-  (db/query ["SELECT  FROM USERS WHERE ID = ?" id]))
+  (first (db/query ["SELECT  FROM USERS WHERE ID = ?" id])))
 
-(defn name-query
+(defn name
   "按照用户名查询用户"
   [name]
-  (db/query ["SELECT  FROM USERS WHERE NAME = ?" name]))
-
-(defn check-name
-  "检测用户名是否存在"
-  [username]
-  (empty? (k/select users
-                    (k/fields :id)
-                    (k/where {:username username}))))
+  (first (db/query ["SELECT  FROM USERS WHERE NAME = ?" name])))
 
 (defn check-name
   "检测用户是否存在"
@@ -44,7 +33,8 @@
   "创建用户"
   [username encrypted-password email]
   (db/insert! :users
-              {:username username
+              {
+                :username username
                :password encrypted-password
                :email    email}))
 
--- src/clojure_china/model/post/post.clj
+++ src/clojure_china/model/post/post.clj
@@ -1,60 +1,121 @@
 (ns clojure-china.model.post.post
   "对于post的一些列操作"
+  (:import (java.security Timestamp)
+           (java.util Date))
   (:require [clojure.java.jdbc :as jdbc]
-            [korma
-             [core :as k]
-             [db :as kdb]]
-            [clojure-china.model.dbconn :refer [db-spec]]
-            [clojure-china.model.entitys :refer :all])
-  (:import [java.sql Timestamp]
-           [java.util Date]))
+            [clj-time.local :as l]
+            [clojure-china.model.dbutil :as db]))
 
 (def page-size 20)
 (def table "CC_POST")
 
 (defn id
+  "
+  按照id搜索post
+  param:
+    id post的id
+  "
   [^Integer id]
-  (first
-    (k/select posts
-              (k/where {:id id}))))
+  (first (db/query
+           ["SELECT  FROM POSTS WHERE ID = ?" id])))
 
-(defn paging-byauthorid
+(defn by-user-id
+  "
+  按照所给user的id来查找post
+  param:
+    userid: 用户的id
+    pages : 所需要的页数
+  "
+  [^Integer userid ^Integer pages]
+  (db/query
+    ["SELECT  FROM POSTS LEFT JOIN USERS ON
+    USERS.ID = POSTS.USER_ID WHERE USERS.ID = ? LIMIT ?, ?"
+     userid (* pages page-size) page-size]))
+
+(defn by-user-id
+  "
+  按照所给user的name来查找post
+  param:
+    username: 用户的username
+    pages   : 所需要的页数
+  "
+  [^Integer username ^Integer pages]
+  (db/query
+    ["SELECT  FROM POST LEFT JOIN USERS ON
+    USERS.ID = POSTS.USER_ID WHERE USERS.USERNAME = ? LIMIT ?, ?"
+     username (* pages page-size) pages]))
+
+(defn by-node-id
+  "
+  按照所给node的id来朝着post
+  param:
+    id    : node的id
+    pages : 所需要的页数
+  "
   [^Integer id ^Integer pages]
-  (k/select posts
-            (k/with users)
-            (k/fields :posts.id :posts.title)
-            (k/where {:users.id id})
-            (k/limit page-size)
-            (k/offset (* pages 20))))
+  (db/query
+    ["SELECT  FROM POST LEFT JOIN NODES ON
+    NODE.ID = POST.NODE_ID WHERE NODES.ID = ? LIMIT ?, ?"
+     id (* pages page-size) page-size]))
 
-(defn paging-byauthorname
-  [^String username ^Integer pages]
-  (k/select posts
-            (k/with users)
-            (k/fields :posts.id :posts.title)
-            (k/where {:users.username username})
-            (k/limit page-size)
-            (k/offset (* pages 20))))
+(defn by-node-name
+  "
+  按照所给node的name来朝着post
+  param:
+    name  : node的name
+    pages : 所需要的页数
+  "
+  [^Integer name ^Integer pages]
+  (db/query
+    ["SELECT  FROM POST LEFT JOIN NODES ON
+    NODE.ID = POST.NODE_ID WHERE NODES.NODENAME = ? LIMIT ?, ?"
+     id (* pages page-size) page-size]))
 
-(defn paging-bynodeid
-  [^Integer id ^Integer pages]
-  (k/select posts
-            (k/with nodes)
-            (k/fields :posts.id :posts.title)
-            (k/where {:nodes.id id})
-            (k/limit page-size)
-            (k/offset (* pages 20))))
+(defn create!
+  "
+  创建post
+  param:
+    title   : post的标题
+    content : post的内容
+    userid  : post的所属用户
+    nodeid  : post的所属节点
+  "
+  [^String title ^String content
+   ^Integer userid ^Integer nodeid]
+  (db/insert! :posts
+              {
+                :title      title
+                :content    content
+                :user_id    userid
+                :node_id    nodeid
+                :created_on (Timestamp. (.getTime (Date.)))
+                :updated_on (Timestamp. (.getTime (Date.)))
+                }))
 
-(defn paging-bynodename
-  [^String nodename ^Integer pages]
-  (k/select posts
-            (k/with nodes)
-            (k/fields :posts.id :posts.title)
-            (k/where {:nodes.node nodename})
-            (k/limit page-size)
-            (k/offset (* pages 20))))
+(defn update!
+  "
+  修改post
+  param:
+    title   : post的标题
+    content : post的内容
+    id      : post的id
+  "
+  [^String title ^String content
+   ^Integer id]
+  (db/update! :posts
+              {
+                :title   title
+                :content content}
+              ["id=?" id]))
 
-(defn ttt []
-  (k/select posts
-            (k/with users)
-            (k/fields :posts.id :posts.title)))
+(defn delete!
+  "
+  删除post
+    prarm:
+    id int post的id
+  "
+  [^Integer id]
+  (db/update! :posts
+              {
+                :status "is_deleted"}
+              ["id = ?" id]))

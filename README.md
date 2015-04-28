1e0e1d41c7147013fc92a1862f7bb998ded5204a
paomian <xpaomian@gmail.com>
2015-04-28 21:40:23 +0800

    Go on

--- README.md
+++ README.md
@@ -1,33 +1,12 @@
-#Clojure-china
+# hhh
 
-- This is the source code of Clojure China website.
-- **Please look at the branch whose name is development**
+FIXME
 
 ## Prerequisites
 
-- [Leiningen][1] 1.7.0 or above
+You will need [Leiningen][] 2.0.0 or above installed.
 
-[1]: https://github.com/technomancy/leiningen
-
-- [JAVA][2] 6 or above
-
-[2]:http://www.oracle.com/technetwork/java/index.html
-
-- [postgresql][3]
-
-[3]: http://www.postgresql.org/
-
-- [clojure][4] 1.4 or above
- 
-[4]:http://clojure.org/
-
-- [redis][5] 2.8.7
-
-[5]:http://redis.io/
-
-##Contact & Contribution
-
-- if you want to join us. you can join the QQ group which number is `130107204`
+[leiningen]: https://github.com/technomancy/leiningen
 
 ## Running
 
@@ -37,4 +16,4 @@ To start a web server for the application, run:
 
 ## License
 
-Copyright © 2014 FIXME
+Copyright Â© 2015 FIXME
--- project.clj
+++ project.clj
@@ -1,17 +1,12 @@
-(defproject clojure-china "0.1.0-SNAPSHOT"
-  :description "This is the source code of Clojure China website."
-  :url "http://clojure-china.org"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]
-                 [org.clojure/java.jdbc "0.3.3"] ;jdbc
-                 [postgresql/postgresql "8.4-702.jdbc4"] ;pgsql client
-                 [com.taoensso/carmine "2.4.6"] ;redis client
-                 [lib-noir "0.8.1"] ;A set of utilities and helpers for building ring apps.
-                 [org.jasypt/jasypt "1.7"] ;JAVA加密包
-                 [hiccup "1.0.5"]
-                 ]
-  :plugins [[lein-ring "0.8.10"]]
-  :ring {:handler clojure-china.handler/app}
+(defproject hhh "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.3.1"]
+                 [ring/ring-defaults "0.1.2"]]
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler hhh.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
